cfbc59c32000662443d603e38dd68a129eb5a787
eisont <rlaclgns321@naver.com>
2023-03-16 17:31:47 +0900

    04-4 useEffect와 useLayoutEffect 훅 이해하기_ActivityIndicator 코어 컴포넌트-p265

--- class/ch04/ch04_4/src/screens/Timer.tsx
+++ class/ch04/ch04_4/src/screens/Timer.tsx
@@ -1,19 +1,31 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react';
+import {StyleSheet, View, Text, Button, ActivityIndicator} from 'react-native';
 
 const Timer = () => {
-  const title = 'Timer';
+  const [loading, setLoading] = useState(true);
+  const toggleLoading = useCallback(() => setLoading(prev => !prev), []);
+  useEffect(() => {
+    const id = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(id);
+  }, [loading]);
+
   return (
     <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+      <Text style={[styles.title]}>Timer</Text>
+      <Text>loading: {loading.toString()}</Text>
+      <Button
+        onPress={toggleLoading}
+        title={loading ? 'stop loading' : 'start loading'}
+      />
+      {loading && <ActivityIndicator size="large" color="#512da8" />}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: '#ef5350'},
-  text: {fontSize: 20, color: 'white'},
+  view: {flex: 1, alignItems: 'center', backgroundColor: '#ef5350'},
+  title: {fontSize: 30, fontWeight: '600'},
 });
 
 export default Timer;
