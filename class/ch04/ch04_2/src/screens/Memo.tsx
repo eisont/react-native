0f450f04df272a10fba54e2eddd17288ccd317ea
eisont <rlaclgns321@naver.com>
2023-03-16 13:45:54 +0900

    04-2 useMemo와 useCallback 훅 이해하기_useMemo 훅-p217

--- class/ch04/ch04_2/src/screens/Memo.tsx
+++ class/ch04/ch04_2/src/screens/Memo.tsx
@@ -1,12 +1,27 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useMemo} from 'react';
+import {FlatList, StyleSheet, View, Text} from 'react-native';
+import * as D from '../data';
+import color from 'color';
+import Person from './Person';
+// import {UseClock} from '../hooks';
+
+const title = 'Memo';
 
 const Memo = () => {
-  const title = 'Memo';
+  // const time = UseClock();
+  const people = useMemo(() => D.makeArray(2).map(D.createRandomPerson), []);
+
   return (
     <View style={[styles.view]}>
       <Text style={[styles.text]}>{title}</Text>
+      <FlatList
+        style={[styles.flatList]}
+        data={people}
+        renderItem={({item}) => <Person person={item} />}
+        keyExtractor={(item, _) => item.id}
+        ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+      />
     </View>
   );
 };
@@ -14,6 +29,11 @@ const Memo = () => {
 const styles = StyleSheet.create({
   view: {flex: 1, padding: 5, backgroundColor: '#0d47a1'},
   text: {fontSize: 20, color: 'white'},
+  flatList: {width: '100%'},
+  itemSeparator: {
+    borderWidth: 1,
+    borderColor: color('#9e9e9e').lighten(0.5).string(),
+  },
 });
 
 export default Memo;
