233d1dac99dd8d4234e0cf7ad43be5a75d1a7f8b
eisont <rlaclgns321@naver.com>
2023-03-16 17:56:18 +0900

    04-4 useEffect와 useLayoutEffect 훅 이해하기_setInterval API 호출하기-p268

--- class/ch04/ch04_4/src/screens/Interval.tsx
+++ class/ch04/ch04_4/src/screens/Interval.tsx
@@ -1,19 +1,73 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
+import {Avatar} from '../components';
+import * as D from '../data';
+
+type IdAndAvatar = Pick<D.IPerson, 'id' | 'avatar'>;
 
 const Interval = () => {
-  const title = 'Interval';
+  const [avatars, setAvatars] = useState<IdAndAvatar[]>([]);
+  const [start, setStart] = useState(true);
+  const toggleStart = useCallback(() => setStart(prev => !prev), []);
+  const clearAvatars = useCallback(() => setAvatars(_ => []), []);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (start) {
+        setAvatars(prev => [
+          ...prev,
+          {id: D.randomId(), avatar: D.randomAvatarUrl()},
+        ]);
+      }
+    }, 1000);
+    return () => clearInterval(id);
+  }, [start]);
+
+  const children = avatars.map(({id, avatar}) => (
+    <Avatar
+      key={id}
+      uri={avatar}
+      size={70}
+      viewStyle={styles.avatarViewStyle}
+    />
+  ));
+
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View style={styles.view}>
+      <View style={styles.topBar}>
+        <Text onPress={toggleStart} style={[styles.topBarText]}>
+          {start ? 'stop' : 'start'}
+        </Text>
+        <Text onPress={clearAvatars} style={[styles.topBarText]}>
+          clear avatars
+        </Text>
+      </View>
+      <Text style={styles.title}> Interval</Text>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle}>
+        {children}
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: '#7e57c2'},
-  text: {fontSize: 20, color: 'white'},
+  view: {flex: 1, alignItems: 'center', backgroundColor: '#7e57c2'},
+  title: {fontSize: 30, fontWeight: '600'},
+  topBar: {
+    width: '100%',
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'space-between',
+    backgroundColor: '#0d47a1',
+  },
+  topBarText: {fontSize: 20, color: 'white'},
+  contentContainerStyle: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  avatarViewStyle: {padding: 5},
 });
 
 export default Interval;
