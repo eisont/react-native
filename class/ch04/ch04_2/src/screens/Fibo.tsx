3fb98635e4f0f19326a7bd8d1f67a0e42b0cdd18
eisont <rlaclgns321@naver.com>
2023-03-16 13:50:57 +0900

    04-2 useMemo와 useCallback 훅 이해하기_useMemo의 메모이제이션 기능-p221

--- class/ch04/ch04_2/src/screens/Fibo.tsx
+++ class/ch04/ch04_2/src/screens/Fibo.tsx
@@ -1,13 +1,37 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useMemo} from 'react';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
+import {fibonacci} from './fibonacci';
+import * as D from '../data';
 
 const title = 'Fibo';
 
 const Fibo = () => {
+  const memoizedFibonacci = useMemo(() => fibonacci, []);
+
+  const fibos = useMemo(
+    () =>
+      D.makeArray(20 + 1).map((_, index) => ({
+        number: index,
+        fibonacci: memoizedFibonacci(index),
+      })),
+    [],
+  );
+
   return (
     <View style={[styles.view]}>
       <Text style={[styles.text]}>{title}</Text>
+      <FlatList
+        contentContainerStyle={[styles.contentContainerStyle]}
+        data={fibos}
+        renderItem={({item}) => (
+          <Text style={[styles.text]}>
+            {item.number} : {item.fibonacci}
+          </Text>
+        )}
+        keyExtractor={item => item.number.toString()}
+      />
     </View>
   );
 };
@@ -15,6 +39,7 @@ const Fibo = () => {
 const styles = StyleSheet.create({
   view: {flex: 1, padding: 5, backgroundColor: '#0d47a1'},
   text: {fontSize: 20, color: 'white'},
+  contentContainerStyle: {alignItems: 'center'},
 });
 
 export default Fibo;
